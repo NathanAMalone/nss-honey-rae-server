b163ea242ff1febe4de821a8c3e9070a2c1bf325
Nathan Malone <nathanamalone@gmail.com>
2022-10-31 16:24:02 -0500

    fixed bug from ch 12

--- repairsapi/views/service_ticket_view.py
+++ repairsapi/views/service_ticket_view.py
@@ -73,15 +73,18 @@ class ServiceTicketView(ViewSet):
 
         service_tickets = []
 
-        if "status" in request.query_params:
-            if request.query_params['status'] == "done":
-                service_tickets = ServiceTicket.objects.filter(date_completed__isnull=False)
-
-            if request.query_params['status'] == "all":
-                service_tickets = ServiceTicket.objects.all()
-
-        else:
+        if request.auth.user.is_staff:
             service_tickets = ServiceTicket.objects.all()
+
+            if "status" in request.query_params:
+                if request.query_params['status'] == "done":
+                    service_tickets = ServiceTicket.objects.filter(date_completed__isnull=False)
+
+                if request.query_params['status'] == "all":
+                    pass
+                
+        else:
+            service_tickets = ServiceTicket.objects.filter(date_completed__isnull=False)
 
         serialized = ServiceTicketSerializer(service_tickets, many=True)
         return Response(serialized.data, status=status.HTTP_200_OK)
